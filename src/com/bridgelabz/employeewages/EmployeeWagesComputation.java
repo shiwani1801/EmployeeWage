ee0db725e7f59855cc76a012d922fc015e5f2bda
shiwani <shiwanipaunikar1@gmail.com>
2023-01-06 12:42:28 -0800

    calculate daily wage for Fulltime and Prattime employee program is added

--- src/com/bridgelabz/employeewages/EmployeeWagesComputation.java
+++ src/com/bridgelabz/employeewages/EmployeeWagesComputation.java
@@ -3,20 +3,26 @@ package com.bridgelabz.employeewages;
 public class EmployeeWagesComputation {
     static final int WAGES_PER_HOUR = 20;
     static final int FULL_DAY_HOUR = 8;
+    static final int PARTTIME_DAY_HOUR = 4;
+
 
     public static void main(String[] args) {
-        //math.floor=round the no. down to nearest integer
-        //math.random=gives values in double thats y we do type casting
-        int employeeCheck = (int) (Math.floor(Math.random() * 10) % 2);
+        int employeeCheck = (int) Math.floor(Math.random() * 10) % 2;
         System.out.println(employeeCheck);
         int dailyWage = 0;
-        if (employeeCheck == 1) {
+        if (employeeCheck == 2) {
             dailyWage = FULL_DAY_HOUR * WAGES_PER_HOUR;
-
             System.out.println("Employee is Present");
+            System.out.println("Employee is Full time");
+        }
+        else if (employeeCheck == 1) {
+            dailyWage =PARTTIME_DAY_HOUR * WAGES_PER_HOUR;
+            System.out.println("Employee is Present");
+            System.out.println("Employee is part time");
         } else {
             System.out.println("Employee is Absent");
 
         }System.out.println("daily wage for Day="+ dailyWage);
     }
 }
+
