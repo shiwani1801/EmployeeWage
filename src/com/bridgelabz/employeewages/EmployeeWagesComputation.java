2d537bb14a25ed4d9e0be24e5a550302fa0298bb
shiwani <shiwanipaunikar1@gmail.com>
2023-01-06 12:35:35 -0800

    employee attendance  program is added

--- src/com/bridgelabz/employeewages/EmployeeWagesComputation.java
+++ src/com/bridgelabz/employeewages/EmployeeWagesComputation.java
@@ -2,7 +2,13 @@ package com.bridgelabz.employeewages;
 
 public class EmployeeWagesComputation {
     public static void main(String[] args) {
-        System.out.println("welcome to employee wages program");
+        System.out.println("Welcome to employee wages program");
+        int employeeCheck = (int) Math.floor(Math.random() * 10) % 2;
+        System.out.println(employeeCheck);
+        if (employeeCheck == 1) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
-
