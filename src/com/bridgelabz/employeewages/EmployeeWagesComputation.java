11bc32d2b7d877c4b5e28f286f6e1ecd826e15f4
shiwani <shiwanipaunikar1@gmail.com>
2023-01-17 13:42:31 -0800

    Added:Refactor code by creating method

--- src/com/bridgelabz/employeewages/EmployeeWagesComputation.java
+++ src/com/bridgelabz/employeewages/EmployeeWagesComputation.java
@@ -1,57 +1,51 @@
 package com.bridgelabz.employeewages;
 
 public class EmployeeWagesComputation {
-    static final int WAGES_PER_HOUR = 20;
-    static final int FULLTIME_DAY_HOUR = 8;
-    static final int PARTTIME_DAY_HOUR = 4;
-    static final int FULL_TIME = 2;
-    static final int PART_TIME = 1;
-    static final int WORKING_DAYS_PER_MONTH = 20;
-    static final int WORKING_HOUR_MONTH = 100;
-    static int MonthlyWage=0;
+    //static variables
+    public static final int IS_FULL_TIME = 1;
+    public static final int IS_PART_TIME = 2;
 
-    public static void main(String[] args) {
-        int dayCount = 1;
-        int workingHour=0;
-        while (dayCount <= WORKING_DAYS_PER_MONTH && workingHour <= WORKING_HOUR_MONTH) {
-            int employeeCheck = (int) Math.floor(Math.random() * 10) % 3;//0,1,2
-            System.out.println(employeeCheck);
-            int dailywage = 0;
-            switch (employeeCheck) {
-                case FULL_TIME:
+    //instance variables
+    public final int empRatePerHr;
+    public final int numberOfWorkingDays;
+    public final int maxWorkingHrs;
 
-                    dailywage = FULLTIME_DAY_HOUR * WAGES_PER_HOUR;
-                    workingHour+=FULLTIME_DAY_HOUR;
-                    System.out.println("Employee is Present");
-                    System.out.println("Employee is Full time");
+    //constructor
+    public EmployeeWagesComputation(int empRaterPerHr,int numOfWorkingDays,int maxWorkingHrs )
+    {
+        this.empRatePerHr = empRaterPerHr;
+        this.numberOfWorkingDays = numOfWorkingDays;
+        this.maxWorkingHrs = maxWorkingHrs;
+    }
+    static void getEmployeeWage() {
+        //variables
+        int salary=0;
+        int empHrs=0;
+        int totalWorkingDays=0;
+        int totalEmpHrs=0;
+        EmployeeWagesComputation employee = new EmployeeWagesComputation(20,20,100);
+        while (totalWorkingDays<=employee.numberOfWorkingDays && totalEmpHrs<=employee.maxWorkingHrs)
+        {
+            totalWorkingDays++;
+            double empCheck = Math.floor(Math.random() * 10) % 3;
+            switch ((int)empCheck) {
+                case IS_FULL_TIME:
+                    System.out.println("Employee is full time present");
+                    empHrs=8;
                     break;
-                case PART_TIME:
-                    dailywage = PARTTIME_DAY_HOUR * WAGES_PER_HOUR;
-                    workingHour+=PARTTIME_DAY_HOUR;
-
-                    System.out.println("Employee is Present");
-                    System.out.println("Employee is Parttime");
-                    break;
-
+                case IS_PART_TIME:
+                    System.out.println("Employee is part time present");
+                    empHrs=4;
                 default:
-
-                    System.out.println("Employee is Absent");
-
+                    System.out.println("Employee is absent");
             }
-            System.out.println("daily wage for Day="+dayCount+ "="+ dailywage);
-            MonthlyWage= dailywage * workingHour;
-            dayCount++;
-
-
+            totalEmpHrs += empHrs;
         }
-
-        System.out.println("Working Hours = " +workingHour);
-        System.out.println("monthly wage="+MonthlyWage);
-
+        salary = (totalEmpHrs * employee.empRatePerHr);
+        System.out.println("Employee Wage for a month is "+salary);
+    }
+    public static void main(String args[]) {
+        System.out.println("Welcome to Employee Computation Program");
+        EmployeeWagesComputation.getEmployeeWage();
     }
 }
-
-
-
-
-
